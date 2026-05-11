05ef8dd38422ac791f4cf6469fa1221db70f1edb
10D <chirss17@gmail.com>
2026-05-11 18:22:28 -0400

    codigo de pago de tarjeta

--- EjerciciosMenus/EjerciccioMenu253.py
+++ EjerciciosMenus/EjerciccioMenu253.py
@@ -1,14 +1,22 @@
 menu = True
+deuda = 100000
 while menu :
     print("--MENU--")
     print("1. Pago Tarjeta de credito")
     print("2. Simulacion de compra")
     print("3. Salir")
 
-    op = int(input("Ingrese su opcion"))
+    op = int(input("Ingrese su opcion : "))
 
     if op == 1 :
         print("Pago Tarjeta de credito")
+        pago = int(input("Ingrese monto de pago :"))
+        if pago >= 0 :
+            if pago <= deuda :
+                deuda = deuda - pago
+                print("Pgo exitoso!! su nuevo saldo es : $",deuda)
+            else :
+                print("El pago exede la deuda")
     elif op == 2 :
         print(" Comprando...")
     elif op == 3:
