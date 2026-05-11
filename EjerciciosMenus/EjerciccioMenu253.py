414e891eb13ddab13a5c03b77c4a39847d910f12
10D <chirss17@gmail.com>
2026-05-11 18:40:19 -0400

    actualizacion

--- EjerciciosMenus/EjerciccioMenu253.py
+++ EjerciciosMenus/EjerciccioMenu253.py
@@ -1,26 +1,33 @@
 menu = True
 deuda = 100000
-while menu :
+
+while menu:
     print("--MENU--")
-    print("1. Pago Tarjeta de credito")
-    print("2. Simulacion de compra")
+    print("1. Pago Tarjeta de Crédito")
+    print("2. Simulación de Compras")
     print("3. Salir")
 
-    op = int(input("Ingrese su opcion : "))
+    op = int(input("Ingrese su opción: "))
 
     if op == 1 :
-        print("Pago Tarjeta de credito")
-        pago = int(input("Ingrese monto de pago :"))
+        print("Pago Tarjeta de Crédito")
+        while True:
+            try:
+                pago = int(input("Ingrese monto de pago: "))
+                break
+            except:
+                print("El valor debe ser numerico!")
         if pago >= 0 :
-            if pago <= deuda :
+            if pago <= deuda:
                 deuda = deuda - pago
-                print("Pgo exitoso!! su nuevo saldo es : $",deuda)
-            else :
-                print("El pago exede la deuda")
+                print("Pago exitoso! su nuevo deuda es: $",deuda)
+            else:
+                print("El pago excede la deuda")
+
     elif op == 2 :
-        print(" Comprando...")
-    elif op == 3:
-        print(" Saliendo")
+        print("Comprando...")
+    elif op == 3: 
+        print("Saliendo...")
         menu = False
     else:
-        print("Opcion invalida")
+        print("Opción no válida!")
